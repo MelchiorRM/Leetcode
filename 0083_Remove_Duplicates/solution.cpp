4e177374bd42a84b8a059b068491cedb33e5541e
MelchiorRM <melchiorrem001@gmail.com>
2025-07-27 12:43:41 +0800

    Adding C++ solutions...

--- 0083_Remove_Duplicates/solution.cpp
+++ 0083_Remove_Duplicates/solution.cpp
@@ -2,8 +2,16 @@
 using namespace std;
 class Solution {
     public:
-        Listnode* removeDuplicates(Listnode* head){
-             
+        ListNode* removeDuplicates(ListNode* head){
+             ListNode* temp=head;
+             while(temp!=nullptr && temp->next!=nullptr){
+                if(temp->val==temp->next->val){
+                    temp->next=temp->next->next;
+                } else {
+                    temp=temp->next;
+                }
+             }
+             return head;
         }
 };
 return 0;
